a53e3eb0561a6a6f38b065c48e307367eec791b9
baseverson <baseverson@gmail.com>
2020-02-23 16:35:00 -0800

    Adding initial PowerBox code

--- venv/src/PowerBox.py
+++ venv/src/PowerBox.py
@@ -3,74 +3,151 @@
 import PowerBoxConfig
 
 try:
+    # Attempt to import the GPIO module for running on Raspberry Pi.
+    # If an exception is caught, then we're not running on a Raspberry Pi.
     import RPi.GPIO as GPIO
     rasp_env = True
 except(ImportError, RuntimeError):
+    # Set flag indicating that we are not running on a Raspberry Pi.
     rasp_env = False
 
+# import flask and json to run/support the lightweight web server and implement the REST API.
 from flask import Flask, jsonify, request
 import json
 
+# Create flask app object
 app = Flask(__name__)
 
-pinList = [4, 17, 27, 22, 5, 6, 13, 19]
-#channelStatus = ['ON', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON', 'ON']
+# pinList contains the Raspberry Pi GPIO pin numbers used for relay channels 1-8
+pinList = {
+    1:4,
+    2:17,
+    3:27,
+    4:22,
+    5:5,
+    6:6,
+    7:13,
+    8:19
+}
+
+# channelStatus holds the current state of each relay channel.  Initialize all channels to "OFF".
 channelStatus = {
     1:"OFF",
     2:"OFF",
-    3: "OFF",
-    4: "OFF",
-    5: "OFF",
-    6: "OFF",
-    7: "OFF",
-    8: "OFF",
+    3:"OFF",
+    4:"OFF",
+    5:"OFF",
+    6:"OFF",
+    7:"OFF",
+    8:"OFF",
 }
 
+# Constants for controlling the Raspberry Pi GPIO pins
+if rasp_env:
+    CHANNEL_ON = GPIO.LOW
+    CHANNEL_OFF = GPIO.HIGH
+
 class PowerBox:
+    """
+    The PowerBox class implements functionality to control an 8-channel relay controllable via a REST API.
+    """
     def __init__(self):
+        """
+        Initializer/Constructor for the PowerBox class.
+
+        Args:
+            :param self: reference back to this object
+
+        Returns:
+            None
+        """
         self.cfg = PowerBoxConfig.PowerBoxConfig()
 
+
     def initialize(self):
+        """
+        Initializes the 8-channel relay.
+
+        Args:
+            :param self: reference back to this object
+
+        Returns:
+            None
+        """
+
+        # If running on the Raspberry Pi, initialize the GPIO pins and set all relay channels to OFF.
         if(rasp_env):
             GPIO.setmode(GPIO.BCM)
-            CHANNEL_ON = GPIO.LOW
-            CHANNEL_OFF = GPIO.HIGH
-            for i in pinList:
-                GPIO.setup(i, GPIO.OUT)
-                GPIO.output(i, GPIO.HIGH)
+            for i in range(1-8):
+                GPIO.setup(pinList[i], GPIO.OUT)
+                GPIO.output(pinList[i], CHANNEL_OFF)
         else:
             print("Running in a test enviroment!")
 
 
     @app.route('/PowerBox/getChannelStatus', methods=['GET'])
     def getChannelStatus():
-        #return jsonify({'channelStatus': channelStatus})
+        """
+        REST API function. Return the current channel status as a JSON.
+
+        Args:
+            None. Handled through the REST request.
+
+        Returns:
+            Current state of all channels as a JSON.
+        """
         return json.dumps(channelStatus)
 
     @app.route('/PowerBox/setChannelState', methods=['POST'])
     def setChannelState():
-        print("setChannelState() called.")
+        """
+        Rest API function. Sets the state of a single relay channel and returns the current state of all channels.
 
+        Args:
+            None. Handled through the REST request.
+
+        Returns:
+            Current state of all channels as a JSON.
+        """
+
+        # Extract the channel number from the REST request
         channel = request.json['channel']
+        # Extract the desired state (On/Off) from the REST request
         state = request.json['state']
-        print("channel: ", channel)
-        print("state: ", state)
+        print("setChannelState request received:")
+        print("    channel: ", channel)
+        print("    state: ", state)
 
-        channelStatus[int(channel)] = state
-
+        # If we're running on a Raspberry Pi, command the GPIO pin.
         if(rasp_env):
             if(state=="ON"):
                 cmd = CHANNEL_ON
             elif(state=="OFF"):
                 cmd = CHANNEL_OFF
             else:
-                return "Invalid channel"
+                return "Invalid channel state"
 
-            GPIO.output(pinList[int(channel)-1],cmd)
+            # Command the GPIO pin.
+            GPIO.output(pinList[int(channel)],cmd)
 
+        # Change the channel state in the local dictionary
+        channelStatus[int(channel)] = state
+
+        # Return the current state for all channels.
         return json.dumps(channelStatus)
 
     def run(self):
+        """
+        Runs the flask webserver
+
+        Args:
+            :param self: reference back to this object
+
+        Returns:
+            None
+        """
+
+        # Run the flask webservice using the address and port defined in the config file.
         app.run(debug=True,host=self.cfg.getAddress(),port=int(self.cfg.getPort()))
 
 if __name__ == '__main__':
